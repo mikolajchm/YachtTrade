1dbca603fb68f707885cb081d6e239d3becbddd7
mikolajchm <chmielewski.5454@gmail.com>
2025-05-10 19:29:50 +0200

    Add login route

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -5,8 +5,9 @@ exports.register = async (req, res) => {
   try {
     const { login, password } = req.body;
     if (login && typeof login === 'string' && password && typeof password === 'string') {
-      const userWithLogin = await User.findOne({ login });
       
+      const userWithLogin = await User.findOne({ login });
+
       if (userWithLogin)  {
         return res.status(409).send({ message: 'User with this login already exist' });
       }
@@ -23,5 +24,27 @@ exports.register = async (req, res) => {
 }
 
 exports.login = async (req, res) => {
+  try {
+    const { login, password } = req.body;
+    if(login && typeof login === 'string' && password && typeof password === 'string') {
 
+      const user = await User.findOne({ login });
+
+      if(!user) {
+        res.status(400).send({ message: 'Login or password is incorrect' });
+      } else {
+        if(bcrypt.compareSync(password, user.password)) {
+          res.status(200).send({ message: 'Login succesfull' });
+        } else {
+          res.status(400).send({ message: 'Login or password is incorrect' });
+        }
+      }
+
+    } else {
+      res.status(400).send({ message: 'Bad request' });
+    }
+
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
 }
