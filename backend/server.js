2c622b9ad72f0a1b84f13890cdd3d735c9d05fa1
mikolajchm <chmielewski.5454@gmail.com>
2025-05-10 17:31:34 +0200

    Add connection to database, add register route, add user model

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,32 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const path = require('node:path');
+
+const authRoutes = require('./routes/auth.routes');
+
+const app = express();
+
+app.listen('8000', () => {
+  console.log('Server is running on port: 8000');
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, '/public')));
+
+mongoose.connect('mongodb://0.0.0.0:27017/YachtTrade');
+const db = mongoose.connection;
+
+db.once('open', () => {
+  console.log('Connected to the database');
+});
+
+db.on('error', err => console.log('Error ' + err));
+
+app.use('/api/auth', authRoutes);
+
+app.use('/', (req, res) => {
+  res.status(404).render('notFound');
+});
