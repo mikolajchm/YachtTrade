6b05449916e8752b2a33eb7440d5daaa8defcdd0
mikolajchm <chmielewski.5454@gmail.com>
2025-05-11 18:55:08 +0200

    Add multipe router for ad module

--- backend/controllers/ads.controller.js
+++ backend/controllers/ads.controller.js
@@ -1,2 +1,62 @@
 const fs = require('fs');
 const path = require('node:path');
+
+const Ad = require('../models/ad.model');
+
+exports.allAds = async (req, res) => {
+  try {
+    return res.json(await Ad.find({}));
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
+};
+
+
+exports.getById = async (req, res) => {
+  try {
+    const ad = await Ad.findById({ _id: req.params.id });
+    if (ad) {
+      return res.json(ad);
+    } else {
+      res.status(404).send({ message: 'Not found' });
+    }
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
+}
+
+
+exports.postAd = async (req, res) => {
+  try {
+
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
+}
+
+
+exports.deleteAd = async (req, res) => {
+  try {
+
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
+}
+
+
+exports.putAd = async (req, res) => {
+  try {
+
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
+}
+
+
+exports.getSearchPharse = async (req, res) => {
+  try {
+
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
+}
---
+++ backend/routes/ads.routes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const authMiddleware = require('../utils/authMiddleware');
+
+const ads = require('../controllers/ads.controller');
+
+router.get('/ads', ads.allAds);
+router.get('/ads/:id', ads.getById);
+router.post('/ads', authMiddleware, ads.postAd);
+router.delete('/ads/:id', authMiddleware, ads.deleteAd);
+router.put('/ads/:id', authMiddleware, ads.putAd);
+
+module.exports = router;
