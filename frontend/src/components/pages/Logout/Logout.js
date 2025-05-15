f38547e6900ea327e1ede3df484271bb7b57d0c6
mikolajchm <chmielewski.5454@gmail.com>
2025-05-15 16:08:03 +0200

    feat: add logout functionality

--- frontend/src/components/pages/Logout/Logout.js
+++ frontend/src/components/pages/Logout/Logout.js
@@ -1,9 +1,27 @@
+import { API_URL } from "../../../config";
+import { useDispatch } from "react-redux";
+import { logOut } from "../../../redux/userRedux";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+
 const Logout = () => {
-  return (
-    <div>
-      <h1>Logout</h1>
-    </div>
-  )
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const options = {
+      method: 'DELETE',
+    };
+
+    fetch(`${API_URL}/auth/logout`, options)
+      .then(() => {
+          dispatch(logOut());
+          navigate('/');
+        })
+  }, [dispatch]);
+
+  return null;
 };
 
 export default Logout;
